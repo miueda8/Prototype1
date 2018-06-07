259218fbd9073d6be6e1fc2c2d99911d026364cc
Mizuki Ueda <miueda@microsoft.com>
2018-06-07 18:04:53 +0900

    Add project files.

---
+++ Prototype1/JsonCreator.cs
@@ -0,0 +1,82 @@
+﻿using AdaptiveCards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype1
+{
+    class JsonCreator
+    {
+        public string CreateJson(string str)
+        {
+            AdaptiveCard card = new AdaptiveCard();
+
+            AdaptiveTextBlock title = new AdaptiveTextBlock
+            {
+                Text = str,
+                Size = AdaptiveTextSize.Medium,
+                Wrap = true
+            };
+
+            //AdaptiveColumnSet columnSet = new AdaptiveColumnSet();
+
+            //AdaptiveColumn photoColumn = new AdaptiveColumn
+            //{
+            //    Width = "auto"
+            //};
+            //AdaptiveImage image = new AdaptiveImage
+            //{
+            //    Url = new Uri("https://pbs.twimg.com/profile_images/587911661526327296/ZpWZRPcp_400x400.jpg"),
+            //    Size = AdaptiveImageSize.Small,
+            //    Style = AdaptiveImageStyle.Person
+            //};
+            //photoColumn.Items.Add(image);
+
+            //AdaptiveTextBlock name = new AdaptiveTextBlock
+            //{
+            //    Text = "Matteo Pagani",
+            //    Weight = AdaptiveTextWeight.Bolder,
+            //    Wrap = true
+            //};
+
+            //AdaptiveTextBlock date = new AdaptiveTextBlock
+            //{
+            //    Text = lastNews.PublishDate.ToShortDateString(),
+            //    IsSubtle = true,
+            //    Spacing = AdaptiveSpacing.None,
+            //    Wrap = true
+            //};
+
+            //AdaptiveColumn authorColumn = new AdaptiveColumn
+            //{
+            //    Width = "stretch"
+            //};
+            //authorColumn.Items.Add(name);
+            //authorColumn.Items.Add(date);
+
+            //columnSet.Columns.Add(photoColumn);
+            //columnSet.Columns.Add(authorColumn);
+
+            AdaptiveTextBlock body = new AdaptiveTextBlock
+            {
+                Text = str,
+                Wrap = true
+            };
+
+            //AdaptiveOpenUrlAction action = new AdaptiveOpenUrlAction
+            //{
+            //    Url = new Uri(lastNews.Link),
+            //    Title = "View"
+            //};
+
+            card.Body.Add(title);
+            //card.Body.Add(columnSet);
+            card.Body.Add(body);
+            //card.Actions.Add(action);
+
+            return card.ToJson();
+        }
+    }
+}
---
+++ Prototype1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using AdaptiveCards.Rendering.Uwp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel.UserActivities;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Prototype1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OnNewTodo(object sender, RoutedEventArgs e)
+        {
+            AdaptiveCardRenderer renderer = new AdaptiveCardRenderer();
+
+            string json = new JsonCreator().CreateJson(TodoTitle.Text);
+            AdaptiveCardParseResult card = AdaptiveCard.FromJson(json);
+            var renderResult = renderer.RenderAdaptiveCard(card.Card);
+
+            if (renderResult != null)
+            {
+                MainPanel.Children.Add(renderResult.FrameworkElement);
+            }
+        }
+    }
+}
