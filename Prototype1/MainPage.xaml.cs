9e5ccd0b7b8380f8f857530f849d3e95a261a996
Mizuki Ueda <miueda@microsoft.com>
2018-06-09 21:35:07 +0900

    added host config

--- Prototype1/MainPage.xaml.cs
+++ Prototype1/MainPage.xaml.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Windows.ApplicationModel.UserActivities;
+using System.Threading.Tasks;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,18 +26,39 @@ namespace Prototype1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        string jsonString;
+
         public MainPage()
         {
             this.InitializeComponent();
+            LoadHostConfig();
+        }
+
+        private async void LoadHostConfig()
+        {
+            Windows.Storage.StorageFolder installedLocation = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Schemas\\HostConfig.json"));
+            using (var inputStream = await file.OpenReadAsync())
+            using (var classicStream = inputStream.AsStreamForRead())
+            using (var streamReader = new StreamReader(classicStream))
+            {
+                while (streamReader.Peek() >= 0)
+                {
+                    jsonString += streamReader.ReadLine();
+                }
+            }
         }
 
         private async void OnNewTodo(object sender, RoutedEventArgs e)
         {
             AdaptiveCardRenderer renderer = new AdaptiveCardRenderer();
 
+            var hostConfig = AdaptiveHostConfig.FromJsonString(jsonString);
+            renderer.HostConfig = hostConfig.HostConfig;
+
             string json = new JsonCreator().CreateJson(TodoTitle.Text);
-            AdaptiveCardParseResult card = AdaptiveCard.FromJson(json);
-            var renderResult = renderer.RenderAdaptiveCard(card.Card);
+            AdaptiveCardParseResult card = AdaptiveCard.FromJsonString(json);
+            var renderResult = renderer.RenderAdaptiveCard(card.AdaptiveCard);
 
             if (renderResult != null)
             {
